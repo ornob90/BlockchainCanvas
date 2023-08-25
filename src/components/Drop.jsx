0cd1907b38f341f9d5b601a2addb4b9aca50b363
ornob90 <oornob49@gmail.com>
2023-08-25 19:59:15 +0600

    never miss a drop responsive complete

--- src/components/Drop.jsx
+++ src/components/Drop.jsx
@@ -4,19 +4,21 @@ import ContentWrapper from "./ContentWrapper";
 const Drop = () => {
   return (
     <div className="flex justify-center items-center w-full  bg-gradient-to-r from-[rgb(14,8,42)] to-[#000000]">
-      <div className="rounded-2xl shadow-lg w-4/5 mx-auto max-w-[1224px] bg-[#0E0E0E] my-20 py-20 flex justify-center flex-col gap-8 items-center text-white">
-        <h1 className="text-4xl font-bold">Never miss a drop</h1>
-        <p className="text-[#7D808C]">
+      <div className="rounded-2xl shadow-lg w-4/5 mx-auto max-w-[1224px] bg-[#0E0E0E] my-20 py-20 flex justify-center flex-col gap-8 items-center text-white lg:px-0 px-4">
+        <h1 className="text-3xl md:text-4xl font-bold">Never miss a drop</h1>
+        <p className="text-[#7D808C] text-center">
           Subscribe to our super-exclusive drop list and be the first to know
           about upcoming drops
         </p>
-        <div className="flex gap-8 w-full justify-center">
+        <div className="flex flex-col sm:flex-row gap-8 w-full justify-center">
           <input
             type="text"
-            className="input bg-[#1A2344] pr-[15%] pl-6 rounded-xl"
+            className="input bg-[#1A2344] pr-[10%] lg:pr-[15%] pl-6 rounded-xl"
             placeholder="Enter your email address"
           />
-          <button className="bg-[#1E50FF] px-7 rounded-2xl">Subscribe</button>
+          <button className="bg-[#1E50FF] rounded-2xl py-2 w-[25%] sm:w-[20%] lg:w-[10%] md:w-[13%] text-sm">
+            Subscribe
+          </button>
         </div>
       </div>
     </div>
